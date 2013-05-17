950456dcb0151bdafe315298cf7a462bb33f4332
xles <xles@mirakulix.org>
2013-05-17 22:31:31 +0200

    Worked on gusto.

--- bin/gusto.php
+++ bin/gusto.php
@@ -1,9 +1,9 @@
 #!/usr/bin/php
 <?php
-if($argc == 1) 
+if ($argc == 1) 
 	exit("Ambiguous command, see help for more info.\n");
 
-switch($argv[1]) {
+switch ($argv[1]) {
 	case 'help':
 	case 'new':
 	case 'remove':
@@ -11,4 +11,42 @@ switch($argv[1]) {
 	default:
 		exit("Unknown command '{$argv[1]}'.\n");
 		break;
+}
+
+function help()
+{
+
+}
+
+function _new()
+{
+	switch ($argv[2]) {
+		case 'post':
+		case 'category':
+		case 'tag':
+		default:
+			break;
+	}
+}
+
+function _remove()
+{
+	switch ($argv[2]) {
+		case 'post':
+		case 'category':
+		case 'tag':
+		default:
+			break;
+	}
+}
+
+function _edit()
+{
+	switch ($argv[2]) {
+		case 'post':
+		case 'category':
+		case 'tag':
+		default:
+			break;
+	}
 }
